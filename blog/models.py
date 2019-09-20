705f76bfbb0ca70b3c2f02bad6906577f2b5dd1c
DongSeon Lee <eastline@maycan.co.kr>
2019-09-20 13:11:21 +0900

    Fix Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post (models.Model):
+    title = models.CharField(max_length=40, help_text='최대 40자 내로 입력가능합니다.')
+    content = models.TextField(verbose_name='주관기관')
+
+    deadline = models.DateTimeField()
+    tag = models.CharField(max_length=100, blank=True)
+
+    autor = models.ForeignKey(User, on_delete=True)
+
+    def __str__(self):
+        return '{}::{}'.format(self.title.self.deadline)
