b9582d3da789fbf0a78e15e61e632bba124a6c1b
DongSeon Lee <eastline@maycan.co.kr>
2019-09-22 22:12:04 +0900

    Post: list view

--- blog/models.py
+++ blog/models.py
@@ -3,13 +3,17 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Post (models.Model):
-    title = models.CharField(max_length=40, help_text='최대 40자 내로 입력가능합니다.')
-    content = models.TextField(verbose_name='주관기관')
+    title = models.CharField(max_length=40, verbose_name='제목')
+    organization = models.CharField(max_length=20, verbose_name='주관기관')
 
-    deadline = models.DateTimeField()
-    tag = models.CharField(max_length=100, blank=True)
+    birthline = models.DateField(verbose_name='시작날짜')
+    deadline = models.DateTimeField(verbose_name='마감날짜')
 
-    autor = models.ForeignKey(User, on_delete=True)
+    tag = models.CharField(max_length=100, blank=True, verbose_name='태그')
+
+    autor = models.ForeignKey(User, on_delete=True, verbose_name='등록자')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='등록일')
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{}::{}'.format(self.title, self.deadline)
