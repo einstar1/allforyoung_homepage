cd96baa5f66f62b389f8bdf4d32b6b93551f202b
DongSeon Lee <eastline@maycan.co.kr>
2019-09-25 10:33:57 +0900

    upload

--- blog/models.py
+++ blog/models.py
@@ -1,19 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+        
+
+        
 class Post (models.Model):
-    title = models.CharField(max_length=40, verbose_name='제목')
+    Category = models.ForeignKey(Category, blank=True, null=True, on_delete=models.SET_NULL, verbose_name='카테고리')
+
+    title = models.CharField(max_length=40, verbose_name='대외활동명')
     organization = models.CharField(max_length=20, verbose_name='주관기관')
+
+    image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True, verbose_name='이미지')
 
     birthline = models.DateField(verbose_name='시작날짜')
     deadline = models.DateTimeField(verbose_name='마감날짜')
-
+  
+   
     tag = models.CharField(max_length=100, blank=True, verbose_name='태그')
+    
+    link = models.TextField(verbose_name='링크')
 
-    autor = models.ForeignKey(User, on_delete=True, verbose_name='등록자')
+    author = models.ForeignKey(User, on_delete=True, verbose_name='등록자')
     created = models.DateTimeField(auto_now_add=True, verbose_name='등록일')
     updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return '{}::{}'.format(self.title, self.deadline)
+        return '{}'.format(self.title)
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,70 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+def create_category(name='대외활동', description=''):
+    category, is_created = Category.objects.get_or_create(
+        name=name,
+        description=description,
+    )
+
+    return category
+
+def create_post(title, organization, author, category=None):
+        blog_post = Post.objects.create(
+            title=title,
+            organization=organization,
+            birthline=birthline,
+            deadline=timezone.now(),
+            link=link,
+            author=author,
+            created=timezone.now(),
+            updated=timezone.now(),
+        )
+
+        return blog_post
 
 # Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='westline', password='nopassword')
+    
+    def test_post_list(self):
+        response = self.client.get('/blog/')
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content,'html.parser')
+        title = soup.title
+
+        self.assertEqual(title.text, '대외활동의 모든 것')
+        
+        menu = soup.find('section', id='menu')
+        self.assertIn('전체', menu.text)
+        self.assertIn('대외활동', menu.text)
+        self.assertIn('동아리', menu.text)
+
+class TestModel(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.author_000 = User.objects.create(username='westline', password='nopassword')
+
+    def test_category(self):
+        category = create_category()
+
+
+    #def test_post(self): 
+        
+        #post_000 = create.post(
+            #title='The first post',
+            #organization='SK',
+            #birthline='2019-05-01',
+            #deadline=timezone.now(),
+            #link='www.naver.com',
+            #author=self.author_000,
+            #created=timezone.now(),
+            #updated=timezone.now(),
+        #)
